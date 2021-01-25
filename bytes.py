4fadf03b7b75cb72547805f6f01a6884a86ed9ac
07-mc <67789575+07-mc@users.noreply.github.com>
2021-01-25 10:12:31 +0800

    Delete bytes.py

--- bytes.py
+++
@@ -1,8 +0,0 @@
-import json
-file = input("file: ")
-list = []
-with open(file, "rb") as f:
-    for byte in f.read():
-        list.append(byte)
-with open("out.json", "w") as out:
-    json.dump(list, out)
